d3902da0ed30ecf828933bcce2329fc1df79b163
Derek759 <dereksadakane1@gmail.com>
2023-04-16 20:30:10 -0300

    Derek 16-04-2023 20h30min

--- assets/js/hamburguer-roll.js
+++ assets/js/hamburguer-roll.js
@@ -1,16 +1,10 @@
 var containerHamburguer = document.querySelector('.container-hamburguer');
-// var menuHamburguer = document.querySelector('.menu-hamburguer');
 
 function definirTopContainerHamburguer(containerHamburguer, valor) {
     if (containerHamburguer && containerHamburguer.style) {
         containerHamburguer.style.top = valor;
     }
 }
-// function definirTopMenuHamburguer(menuHamburguer, valor) {
-//     if (menuHamburguer && menuHamburguer.style) {
-//         menuHamburguer.style.top = valor;
-//     }
-// }
 
 var altura = window.innerHeight + 61
 var scrollHeight = window.innerHeight; 
@@ -25,32 +19,3 @@ window.addEventListener('scroll', function() {
         definirTopContainerHamburguer(containerHamburguer, 6.1 + "rem");
     }
 });
-
-// window.addEventListener('scroll', function() {
-//     if (window.pageYOffset <= scrollHeight) {
-//         menuHamburguer.classList.remove('fixed');
-//         definirTopMenuHamburguer(menuHamburguer, 0 + "px");
-//     } else {
-//         menuHamburguer.classList.add('fixed');
-//         definirTopMenuHamburguer(menuHamburguer, 0);
-//     }
-// });
-
-
-
-
-
-// function obterPosicaoElemento(elemento) {
-//     var posicao = 0;
-//     while (elemento) {
-//         posicao = (elemento.offsetTop - window.pageYOffset);
-//         elemento = elemento.offsetParent;
-//     }
-//     return posicao;
-// }
-
-// var meuElemento = document.getElementById("section-comidas");
-// var posicao = obterPosicaoElemento(meuElemento);
-// console.log("Posição topo: " + posicao + "px");
-
-// console.log(meuElemento.offsetTop)
