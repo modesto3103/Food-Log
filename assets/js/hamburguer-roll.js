486e17847952489f49f22f4f7670d1a3a56c7f0c
Derek759 <dereksadakane1@gmail.com>
2023-04-16 20:16:52 -0300

    Derek 16-04-2023 20h16min

--- assets/js/hamburguer-roll.js
+++ assets/js/hamburguer-roll.js
@@ -1,55 +1,56 @@
-var iconHamburguer = document.querySelector('.icon-hamburguer');
-var menuHamburguer = document.querySelector('.menu-hamburguer');
+var containerHamburguer = document.querySelector('.container-hamburguer');
+// var menuHamburguer = document.querySelector('.menu-hamburguer');
 
-function definirTopIconHamburguer(iconHamburguer, valor) {
-    if (iconHamburguer && iconHamburguer.style) {
-        iconHamburguer.style.top = valor;
+function definirTopContainerHamburguer(containerHamburguer, valor) {
+    if (containerHamburguer && containerHamburguer.style) {
+        containerHamburguer.style.top = valor;
     }
 }
-function definirTopMenuHamburguer(menuHamburguer, valor) {
-    if (menuHamburguer && menuHamburguer.style) {
-        menuHamburguer.style.top = valor;
-    }
-}
+// function definirTopMenuHamburguer(menuHamburguer, valor) {
+//     if (menuHamburguer && menuHamburguer.style) {
+//         menuHamburguer.style.top = valor;
+//     }
+// }
 
-var altura = window.innerHeight + 20
+var altura = window.innerHeight + 61
 var scrollHeight = window.innerHeight; 
 
 window.addEventListener('scroll', function() {
     if (window.pageYOffset <= scrollHeight) {
-        iconHamburguer.classList.remove('fixed');
-        definirTopIconHamburguer(iconHamburguer, altura + "px");
+        console.log(window.pageYOffset, scrollHeight);
+        containerHamburguer.classList.remove('fixed');
+        definirTopContainerHamburguer(containerHamburguer, altura + "px");
     } else {
-        iconHamburguer.classList.add('fixed');
-        definirTopIconHamburguer(iconHamburguer, 2 + "rem");
+        containerHamburguer.classList.add('fixed');
+        definirTopContainerHamburguer(containerHamburguer, 6.1 + "rem");
     }
 });
 
-window.addEventListener('scroll', function() {
-    if (window.pageYOffset <= scrollHeight) {
-        menuHamburguer.classList.remove('fixed');
-        definirTopMenuHamburguer(menuHamburguer, 0 + "px");
-    } else {
-        menuHamburguer.classList.add('fixed');
-        definirTopMenuHamburguer(menuHamburguer, 0);
-    }
-});
+// window.addEventListener('scroll', function() {
+//     if (window.pageYOffset <= scrollHeight) {
+//         menuHamburguer.classList.remove('fixed');
+//         definirTopMenuHamburguer(menuHamburguer, 0 + "px");
+//     } else {
+//         menuHamburguer.classList.add('fixed');
+//         definirTopMenuHamburguer(menuHamburguer, 0);
+//     }
+// });
 
 
 
 
 
-function obterPosicaoElemento(elemento) {
-    var posicao = 0;
-    while (elemento) {
-        posicao = (elemento.offsetTop - window.pageYOffset);
-        elemento = elemento.offsetParent;
-    }
-    return posicao;
-}
+// function obterPosicaoElemento(elemento) {
+//     var posicao = 0;
+//     while (elemento) {
+//         posicao = (elemento.offsetTop - window.pageYOffset);
+//         elemento = elemento.offsetParent;
+//     }
+//     return posicao;
+// }
 
-var meuElemento = document.getElementById("section-comidas");
-var posicao = obterPosicaoElemento(meuElemento);
-console.log("Posição topo: " + posicao + "px");
+// var meuElemento = document.getElementById("section-comidas");
+// var posicao = obterPosicaoElemento(meuElemento);
+// console.log("Posição topo: " + posicao + "px");
 
-console.log(meuElemento.offsetTop)
+// console.log(meuElemento.offsetTop)
